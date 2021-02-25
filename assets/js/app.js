03eba538fe5637a4ca43860294737d7a46cda0ea
Jānis Albertiņš <albertins@caballero.lv>
2021-02-25 19:40:31 +0200

    Sort & filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ $(function() {
 
 		init: function () {
 			App.loadJson();
-			App.clickEvents();
+			App.events();
 		},
 
 		loadJson: function () {
@@ -33,6 +33,8 @@ $(function() {
 						'<div class="units"></div>' +
 					'<span class="sorter"></span></th>' );
 				$('.body tr').append( '<td class="metric" data-metric="'+metric.id+'"></td>' );
+				$('.filters').append( '<label class="filters-checkbox">'+
+					'<input type="checkbox" name="'+metric.id+'" checked />'+metric.name+'</label>' );
 				metric.units.forEach(function( unit ){
 					var selected = unit.selected ? 'checked' : '';
 					$('.body tr td:last').append('<div data-unit="'+unit.id+'" class="metric-unit '+selected+'"></div>');
@@ -54,25 +56,69 @@ $(function() {
 					$('[data-unit="'+sensorId+'"]', row).text(metrics.v);
 				});
 			});
+			App.sorter();
 		},
 		
-		clickEvents: function(){
+		events: function(){
 			$('body').on('click', '.sorter', function(){
 				$('.sorter').not(this).removeClass().addClass('sorter');
-				if( $(this).hasClass('sorter--desc') ){
-					$(this).removeClass('sorter--desc');
-				} else if( $(this).hasClass('sorter--asc') ){
+				if( $(this).hasClass('sorter--asc') ){
 					$(this).removeClass('sorter--asc').addClass('sorter--desc');
 				} else {
-					$(this).addClass('sorter--asc');
+					$(this).removeClass('sorter--desc').addClass('sorter--asc');
 				}
+				App.sorter();
 			});
 			$('body').on('change', '.metric-unit__label input', function(){
 				var shouldChange = $('.body [data-unit="'+$(this).closest('.units').find('input:checked').attr('val')+'"]');
 				shouldChange.addClass('checked').prevAll().removeClass('checked');
 				shouldChange.nextAll().removeClass('checked');
 			});
-		}
+
+			$('body').on('change', '.filters-checkbox input', function(){
+				$('.metric[data-metric="'+$(this).attr('name')+'"]').toggleClass('hidden', ! $(this).is(':checked'));
+				if( ! $('th:not(.hidden) .sorter--desc, th:not(.hidden) .sorter--asc').length ){
+					$('.head .name .sorter').addClass('sorter--asc');
+					App.sorter();
+				}
+			});
+
+			$('#filter').on('change keyup', App.filter);
+		},
+
+		sorter: function(){
+			var rows = $('.body tr').get();
+			var column = $('.sorter--desc, .sorter--asc').closest('th').attr('data-metric');
+			rows.sort(function(a, b) {
+				if( ! column ){
+					var A = $(a).find('.name').eq(0).text().toUpperCase();
+					var B = $(b).find('.name').eq(0).text().toUpperCase();
+				} else {
+					var A = $(a).find('.metric[data-metric="'+column+'"] .checked').eq(0).text().toUpperCase();
+					var B = $(b).find('.metric[data-metric="'+column+'"] .checked').eq(0).text().toUpperCase();
+				}
+				if (A < B) {
+					return $('.sorter--asc').length ? -1 : 1;
+				}
+				if (A > B) {
+					return $('.sorter--asc').length ? 1 : -1;
+				}
+				return 0;
+			});
+
+			$.each(rows, function(index, row) {
+				$('.body').append(row);
+			});
+		},
+
+		filter: function(){
+	        var searched = $('#filter').val().trim().toLowerCase().split(' ');
+	        var search = new RegExp( '^(?=.*(' + searched.join('))(?=.*?(') + ')).*' );
+	        $('.body .name').each(function(){
+	            var txt = $(this).text().toLowerCase();
+	            $(this).closest('tr').toggleClass( 'hidden', txt.match( search ) === null );
+	        });
+	    },
 	};
 
 	App.init();
