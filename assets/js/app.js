db416c2dc21dddc20f1a5198c5a6353c518b7e0a
Jānis Albertiņš <albertins@caballero.lv>
2021-02-25 13:38:57 +0200

    Metrics toggling.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ $(function() {
 
 		init: function () {
 			App.loadJson();
+			App.clickEvents();
 		},
 
 		loadJson: function () {
@@ -27,12 +28,16 @@ $(function() {
 
 		generateTable: function(){
 			App.data.metrics.data.items.forEach(function( metric ){
-				$('.head tr').append( '<th class="metric" data-metric="'+metric.id+'"><div>'+metric.name+'</div></th>' );
+				$('.head tr').append( '<th class="metric" data-metric="'+metric.id+'">' +
+						'<div class="metric">'+metric.name+'</div>' +
+						'<div class="units"></div>' +
+					'<span class="sorter"></span></th>' );
 				$('.body tr').append( '<td class="metric" data-metric="'+metric.id+'"></td>' );
 				metric.units.forEach(function( unit ){
-					var selected = unit.selected ? 'active' : '';
+					var selected = unit.selected ? 'checked' : '';
 					$('.body tr td:last').append('<div data-unit="'+unit.id+'" class="metric-unit '+selected+'"></div>');
-					$('.head tr th:last').append('<div data-unit="'+unit.id+'" class="metric-unit '+selected+'">'+unit.name+'</div>');
+					$('.head tr th:last .units').append('<label data-unit="'+unit.id+'" class="metric-unit__label">'+
+						'<input type="radio" name="'+metric.id+'" val="'+unit.id+'" '+selected+' />'+unit.name+'</label>');
 				});
 			});
 			var clone = $('.body tr').clone();
@@ -48,6 +53,24 @@ $(function() {
 				Object.entries(sensor.metrics).forEach(function([sensorId, metrics]){
 					$('[data-unit="'+sensorId+'"]', row).text(metrics.v);
 				});
+			});
+		},
+		
+		clickEvents: function(){
+			$('body').on('click', '.sorter', function(){
+				$('.sorter').not(this).removeClass().addClass('sorter');
+				if( $(this).hasClass('sorter--desc') ){
+					$(this).removeClass('sorter--desc');
+				} else if( $(this).hasClass('sorter--asc') ){
+					$(this).removeClass('sorter--asc').addClass('sorter--desc');
+				} else {
+					$(this).addClass('sorter--asc');
+				}
+			});
+			$('body').on('change', '.metric-unit__label input', function(){
+				var shouldChange = $('.body [data-unit="'+$(this).closest('.units').find('input:checked').attr('val')+'"]');
+				shouldChange.addClass('checked').prevAll().removeClass('checked');
+				shouldChange.nextAll().removeClass('checked');
 			});
 		}
 	};
